860f775e742c9f1d9e78dcd4ab73816d431a2654
Alex Jackson <alex@alexj.org>
2017-07-13 18:22:44 +0100

    Add test target for LibSpotijack

---
+++ LibSpotijackTests/LibSpotijackTests.swift
@@ -0,0 +1,16 @@
+//
+//  LibSpotijackTests.swift
+//  LibSpotijackTests
+//
+//  Created by Alex Jackson on 13/07/2017.
+//  Copyright © 2017 Alex Jackson. All rights reserved.
+//
+
+import XCTest
+@testable import LibSpotijack
+
+class LibSpotijackTests: XCTestCase {
+    func testTest() {
+        XCTAssert(true == true)
+    }
+}
