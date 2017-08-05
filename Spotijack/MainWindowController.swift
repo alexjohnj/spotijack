0b16357a5d3f1694786f1b243df774f1120840e9
Alex Jackson <alex@alexj.org>
2017-08-05 14:10:07 +0100

    Set up notification registration in main window controller

--- Spotijack/MainWindowController.swift
+++ Spotijack/MainWindowController.swift
@@ -7,6 +7,8 @@
 //
 
 import Cocoa
+import LibSpotijack
+import TypedNotification
 
 class MainWindowController: NSWindowController {
     //MARK: Interface Builder Outlets
@@ -15,17 +17,71 @@ class MainWindowController: NSWindowController {
     @IBOutlet weak var recordButton: NSButton!
     @IBOutlet weak var muteButton: NSButton!
 
+    //MARK: Notification Observer Tokens
+    private var _muteStateDidChangeObserver: NotificationObserver? = nil
+    private var _didEncounterErrorObserver: NotificationObserver? = nil
+    private var _recordingStateDidChangeObserver: NotificationObserver? = nil
+    private var _trackDidChangeObserver: NotificationObserver? = nil
+
     //MARK: Window Lifecycle
     override func windowDidLoad() {
         super.windowDidLoad()
-    
-        print("Hello world from MainWindowController")
-    }
 
-    //MARK: Interface Actions
+        registerForSpotijackNotifications()
+    }
+}
+
+//MARK: UI Actions
+extension MainWindowController {
     @IBAction func recordButtonClicked(_ sender: NSButton) {
     }
 
     @IBAction func muteButtonClicked(_ sender: NSButton) {
+    }
+}
+
+//MARK: Notification Handling
+extension MainWindowController {
+    private func muteStateDidChange(noti: MuteStateDidChange) {
+    }
+
+    private func recordingStateDidChange(noti: RecordingStateDidChange) {
+    }
+
+    private func trackDidChange(noti: TrackDidChange) {
+    }
+
+    private func didEncounterError(noti: DidEncounterError) {
+    }
+
+    /// Registers the window controller for notifications posted by
+    /// SpotijackSessionManager
+    private func registerForSpotijackNotifications() {
+        let sessionManager = SpotijackSessionManager.shared
+        let notificationCenter = sessionManager.notiCenter
+
+        _muteStateDidChangeObserver = notificationCenter.addObserver(
+            forType: MuteStateDidChange.self,
+            object: sessionManager,
+            queue: .main,
+            using: { [weak self] noti in self?.muteStateDidChange(noti: noti) })
+
+        _didEncounterErrorObserver = notificationCenter.addObserver(
+            forType: DidEncounterError.self,
+            object: sessionManager,
+            queue: .main,
+            using: { [weak self] noti in self?.didEncounterError(noti: noti) })
+
+        _trackDidChangeObserver = notificationCenter.addObserver(
+            forType: TrackDidChange.self,
+            object: sessionManager,
+            queue: .main,
+            using: { [weak self] noti in self?.trackDidChange(noti: noti) })
+
+        _recordingStateDidChangeObserver = notificationCenter.addObserver(
+            forType: RecordingStateDidChange.self,
+            object: sessionManager,
+            queue: .main,
+            using: { [weak self] noti in self?.recordingStateDidChange(noti: noti) })
     }
 }
