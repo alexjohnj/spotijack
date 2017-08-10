c3047d0a73685342c8cb3347de1a96dbae43d8db
Alex Jackson <alex@alexj.org>
2017-08-10 16:37:08 +0100

    Create Audio hijack application mock

---
+++ LibSpotijackTests/Mocks/MockAudioHijackApplication.swift
@@ -0,0 +1,51 @@
+//
+//  MockAudioHijackApplication.swift
+//  LibSpotijackTests
+//
+//  Created by Alex Jackson on 10/08/2017.
+//  Copyright © 2017 Alex Jackson. All rights reserved.
+//
+
+import Foundation
+import ScriptingBridge
+import LibSpotijack
+
+internal class MockAudioHijackApplication: NSObject {
+    var _activated: Bool = false
+    var _sessions: SBElementArray
+
+    init(sessions: [MockSpotijackAudioHijackApplicationSession]) {
+        _sessions = SBElementArray(array: sessions)
+    }
+}
+
+// MARK: - SBApplicationProtocol Conformance
+extension MockAudioHijackApplication: SBApplicationProtocol {
+    func activate() {
+        _activated = true
+    }
+
+    var delegate: SBApplicationDelegate! {
+        get {
+            fatalError("Not implemented")
+        }
+        set(newValue) {
+            fatalError("Not implemented")
+        }
+    }
+
+    var isRunning: Bool {
+        return true
+    }
+
+    func get() -> Any! {
+        fatalError("Not implemented")
+    }
+}
+
+// MARK: - AudioHijackApplicationConformance
+extension MockAudioHijackApplication: AudioHijackApplication {
+    func sessions() -> SBElementArray {
+        return _sessions
+    }
+}
