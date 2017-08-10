2c460bc026fef0f09e2285797c2a9d3b19e8ea65
Alex Jackson <alex@alexj.org>
2017-08-10 20:39:49 +0100

    Add unit tests for accessing current Spotify track

--- LibSpotijackTests/SpotijackSession Tests/SpotijackSessionSpotifyTests.swift
+++ LibSpotijackTests/SpotijackSession Tests/SpotijackSessionSpotifyTests.swift
@@ -7,7 +7,34 @@
 //
 
 import XCTest
+@testable import LibSpotijack
 
 /// Tests how SpotijackSession controls Spotify's state
 internal class SpotijackSessionSpotifyTests: XCTestCase {
+    func testGetCurrentTrack() {
+        let expectedTrack = TestTrack.LetTheFlamesBegin._backingTrack
+        let (session, _, _) = SpotijackSession.makeStandardApplications()
+
+        XCTAssertNotNil(session.currentTrack)
+        XCTAssertEqual(session.currentTrack, expectedTrack)
+    }
+
+    func testChangeTrackNotifiesDelegate() {
+        let expectedTrack = TestTrack.FakeHappy._backingTrack
+        var receivedTrack: StaticSpotifyTrack?
+        let delegate = MockSpotijackSessionDelegate()
+        delegate.onSessionDidChangeToTrack = { (_, newTrack) in receivedTrack = newTrack }
+
+        let (session, spotify, _) = SpotijackSession.makeStandardApplications()
+        session.delegate = delegate
+
+        spotify.nextTrack()
+
+        // Simulate a polling
+        session.pollSpotify()
+        session.pollAudioHijackPro()
+
+        XCTAssertNotNil(receivedTrack)
+        XCTAssertEqual(receivedTrack, expectedTrack)
+    }
 }
