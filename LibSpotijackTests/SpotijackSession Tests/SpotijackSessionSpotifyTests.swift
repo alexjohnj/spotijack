452ca964e633cc9014459607defccb6d67e98c8e
Alex Jackson <alex@alexj.org>
2017-08-10 20:29:15 +0100

    Create SpotijackSessionSpotifyTests

---
+++ LibSpotijackTests/SpotijackSession Tests/SpotijackSessionSpotifyTests.swift
@@ -0,0 +1,13 @@
+//
+//  SpotijackSessionSpotifyTests.swift
+//  LibSpotijackTests
+//
+//  Created by Alex Jackson on 10/08/2017.
+//  Copyright © 2017 Alex Jackson. All rights reserved.
+//
+
+import XCTest
+
+/// Tests how SpotijackSession controls Spotify's state
+internal class SpotijackSessionSpotifyTests: XCTestCase {
+}
