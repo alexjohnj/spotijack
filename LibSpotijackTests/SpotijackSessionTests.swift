25d0cb30ffcaff2c58efb53e0727463c32e37f95
Alex Jackson <alex@alexj.org>
2017-08-10 19:53:34 +0100

    Add unit tests for session muting

--- LibSpotijackTests/SpotijackSessionTests.swift
+++ LibSpotijackTests/SpotijackSessionTests.swift
@@ -21,3 +21,39 @@ extension SpotijackSession {
     }
 }
 
+internal class SpotijackSessionTests: XCTestCase {
+    // MARK: - Mute Tests
+    func testGetSessionMuteState() {
+        let expectedMuteState = true
+
+        let (session, _, ahp) = SpotijackSession.makeStandardApplications()
+        ahp._sessions.first(where: { $0._name == "Spotijack" })?._speakerMuted = expectedMuteState
+
+        XCTAssertEqual(session.isMuted, expectedMuteState)
+    }
+
+    func testSetSessionMuteState() {
+        let expectedMuteState = true
+        let (session, _, ahp) = SpotijackSession.makeStandardApplications()
+        let spotijackAHPSession = ahp._sessions.first(where: { $0._name == "Spotijack" })!
+
+        session.isMuted = expectedMuteState
+
+        XCTAssertEqual(spotijackAHPSession._speakerMuted, expectedMuteState)
+    }
+
+    func testChangeMuteStateNotifiesDelegate() {
+        let expectedMuteState = true
+        var receivedMuteState: Bool?
+
+        let mockDelegate = MockSpotijackSessionDelegate()
+        mockDelegate.onSessionDidMute = { (_, isMuted) in receivedMuteState = isMuted }
+
+        let (session, _, _) = SpotijackSession.makeStandardApplications()
+        session.delegate = mockDelegate
+
+        session.isMuted = expectedMuteState
+
+        XCTAssertEqual(expectedMuteState, receivedMuteState)
+    }
+}
