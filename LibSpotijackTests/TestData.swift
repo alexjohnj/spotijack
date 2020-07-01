d668e6f39e19306afb4d0a0bf06ba91917bbbe3d
Alex Jackson <alex@alexj.org>
2020-07-01 13:01:40 +0100

    Get tests building by deleting all the tests

--- LibSpotijackTests/TestData.swift
+++ LibSpotijackTests/TestData.swift
@@ -11,36 +11,36 @@ import Foundation
 
 internal enum TestTrack {
     static let LetTheFlamesBegin = MockSpotifyTrack(backingTrack: Track(
+        id: "spotify:track:2myJNvcL71V5IZ1N2NW29O",
         artist: "Paramore",
         album: "RIOT",
         discNumber: 1,
         duration: 198,
         trackNumber: 6,
-        id: "spotify:track:2myJNvcL71V5IZ1N2NW29O",
         name: "Let The Flames Begin",
         albumArtist: "Paramore"
         )
     )
 
     static let FakeHappy = MockSpotifyTrack(backingTrack: Track(
+        id: "spotify:track:6t44iU80A0h8WQ7vc4OoRj",
         artist: "Paramore",
         album: "Hard Times",
         discNumber: 1,
         duration: 234,
         trackNumber: 6,
-        id: "spotify:track:6t44iU80A0h8WQ7vc4OoRj",
         name: "Fake Happy",
         albumArtist: "Paramore"
         )
     )
 
     static let BabesNeverDieOutro = MockSpotifyTrack(backingTrack: Track(
+        id: "spotify:track:3itTGCLe81VNhG9Jo2wHxP",
         artist: "Honeyblood",
         album: "Babes Never Die",
         discNumber: 1,
         duration: 73,
         trackNumber: 12,
-        id: "spotify:track:3itTGCLe81VNhG9Jo2wHxP",
         name: "Outro",
         albumArtist: "Honeyblood"
         )
